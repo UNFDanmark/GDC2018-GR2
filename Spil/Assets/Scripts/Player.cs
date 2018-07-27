a7ec3af27b012a6e8ffd952cb2df07207ec9ee3f
DESKTOP-B54F68T\UNFGRUPPE2PC1 <teknisk@game.unf.dk>
2018-07-27 21:40:11 +0200

    Player kan bevæge sig frit og zombier følgerefter spiller

---
+++ Spil/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed_player = 10f;
+    public float rotationSpeed_player = 10f;
+    public int health_player = 100;
+    public int exp_player = 0;
+    public int lvl_player = 1;
+    public Rigidbody player;
+
+
+    public float health_regen_sek_player = 1;
+    public int vomit_player_regen_sek = 1;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(0, rotationSpeed_player * Input.GetAxis("Horizontal") * Time.deltaTime, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        Move(speed_player * Input.GetAxis("Vertical"));
+    }
+
+    public void Move(float speed)
+    {
+        player.velocity = (Vector3.up * player.velocity.y) + (transform.forward * speed);
+      
+    }
+}
---
+++ Spil/Assets/Scripts/Zombier.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombier : MonoBehaviour
+{
+
+    public float speed_zombie = 0.5f;
+    public Transform player;
+    public Vector3 direction_zombie;
+    public Rigidbody zombiebody;
+ 
+    // Use this for initialization
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction_zombie = player.position - transform.position;
+        Move_Z(direction_zombie);
+    }
+
+    public void Move_Z(Vector3 a) {
+
+        //zombiebody.velocity = a + (enhed_vector * speed_zombie);
+        //Mulighed for at gøre farten uafhængig af afstanden til player.
+        zombiebody.velocity = a * speed_zombie;
+
+
+    }
+
+
+}
