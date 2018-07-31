dbc0d85a28cf0dcebc2a37d97bc038fd58f67a1b
t520-87137\user <lyd@gdc.dk>
2018-07-31 17:53:54 +0200

    lavet en ekstra scene til lyd

---
+++ Spil/Assets/Scripts/SoundZombie.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundZombie : MonoBehaviour {
+    public AudioClip[] zombie;
+    public AudioSource audioSource;//Vores audioSource
+
+
+
+    public void zombiewalk()
+    {
+        int i = Random.Range(0, zombie.Length); // Random Generator
+        audioSource.clip = zombie[i]; //Sæt lydklipppet i audioSource til den randomvalgte værdi
+        if (!audioSource.isPlaying) //Tjekker om lyden allerede afspilles 
+        {
+            audioSource.Play(); //Afspiller lyden
+        }
+
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
